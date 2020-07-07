1a0357acfd5971cdb1264ddfbbc55cc53bd0c609
Jason Kramer <jasonkramer525@gmail.com>
2020-07-07 00:10:34 -0700

    Changed name of chat

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -35,7 +35,7 @@ export default {
     ...mapState('store', ['userDetails']),
     title() {
       let currentPath = this.$route.fullPath
-      if(currentPath == '/') return 'SmackChat'
+      if(currentPath == '/') return 'KramerChat'
       else if(currentPath.includes('/chat')) return this.otherUserDetails.name
       else if(currentPath == '/auth') return 'Login'
     } 
